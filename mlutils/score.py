24f61cbb433fa19dd93e404215f456707e6613c8
g.platz <g-platz@hotmail.de>
2021-04-16 15:33:08 +0200

    added score with tests

---
+++ mlutils/score.py
@@ -0,0 +1,148 @@
+from typing import Any
+from typing import Dict
+from typing import List
+
+import numpy as np
+from scipy import stats
+from sklearn import metrics
+
+
+def l2_norm(x: np.array, x_hat: np.array) -> np.array:
+    """Calculates the l2-norm (euclidean distance) for x and x_hat.
+
+    Arguments:
+        x (np.array): ground truth.
+        x_hat (np.array): prediction.
+
+    Returns:
+        np.array: Returns the l2-norm between x and x_hat.
+    """
+    r = np.sqrt(np.sum((np.array(x) - np.array(x_hat))**2, axis=-1))
+    return r.reshape(-1, 1)
+
+
+def get_threshold(*, x: np.array, p: float = 100) -> float:
+    """Returns the `perc`% max.
+
+    Arguments:
+        x  (np.array): Input
+        p (float): Percentage (0-100).
+
+    Returns:
+        float: Returns the threshold at `perc`% max.
+    """
+    return stats.scoreatpercentile(x, p)
+
+
+def apply_threshold(*, x, t) -> np.array:
+    """Applies `threshold t` to `x`. Values greater than the `threshold`
+    are 1 and below or equal are 0.
+
+    Arguments:
+        x (np.array): Input array.
+        t (float): Threshold.
+
+    Returns:
+        np.array: Returns the result of the threshold operation.
+    """
+    return np.where(x > t, 1, 0)
+
+
+def eval_softmax(y: np.array) -> np.array:
+    """Turns a multi-class softmax prediction into class labels.
+
+    Arguments:
+        y (np.array): Array with softmax probabilites
+
+    Returns:
+        np.array: Returns an array of shape (x, 1) with the class labels.
+    """
+    return np.argmax(y, axis=-1).reshape(-1, 1)
+
+
+def eval_sigmoid(*, y: np.array, invert: bool = False) -> np.array:
+    """Turns a binary-class sigmoid prediction into 0-1 class labels.
+
+    Args:
+        y (np.array): Array with sigmoid probabilities
+        invert (bool): Whether to invert the labels. (0->1, 1->0)
+
+    Returns:
+        np.array: Returns the binary class labels.
+    """
+    y = (y > 0.5) * 1.
+    if not invert:
+        return y.reshape(-1, 1)
+    else:
+        return (1. - y).reshape(-1, 1)
+
+
+def moving_average(X: np.array, step: int = 10, mode='full') -> np.array:
+    """Calculates the moving average for X with stepsize `step`.
+
+    Args:
+        X (np.array): 1-dimensional array.
+        step (int, optional): Stepsize. Defaults to 10.
+        mode (str, optional): Mode, see np.convolve. Defaults to 'full'.
+
+    Returns:
+        np.array: Returns the moving average.
+    """
+    return np.convolve(X, np.ones((step,)) / step, mode=mode)
+
+
+def eval_metrics(y: np.array, y_hat: np.array) -> Dict[str, Any]:
+    """Calculates accuracy, f1, precision, recall and AUC scores.
+
+    Arguments:
+        y (np.array): Ground truth.
+        y_hat (np.array): Predictions.
+
+    Returns:
+        Dict[str, Any]: Returns a dict with all scores.
+    """
+    scores = {
+        'accuracy': metrics.accuracy_score,
+        'precision': metrics.precision_score,
+        'recall': metrics.recall_score,
+        'f1': metrics.f1_score,
+        'AUC': metrics.roc_auc_score
+    }
+
+    results = {}
+    length = min(len(y), len(y_hat))
+    try:
+        for key in scores:
+            results[key] = scores[key](y[:length], y_hat[:length])
+    except Exception:
+        pass
+    finally:
+        return results
+
+
+def eval_metrics_all(y: List[np.array],
+                     y_hat: List[np.array]) -> Dict[str, Any]:
+    """Calculates combined accuracy, f1, precision, recall and AUC scores for
+    multiple arrays. The arrays are shorted to the minimum length of the
+    corresponding partner and stacked on top of each other to calculated the
+    combined scores.
+
+    Arguments:
+        y (np.array): Ground truth.
+        y_hat {array-like} -- Prediction
+
+    Returns:
+        Dict[str, Any]: Returns a dict with all scores.
+    """
+    y_ = []
+    y_hat_ = []
+
+    for (x, xx) in zip(y, y_hat):
+        length = min(len(x), len(xx))
+        y_.append(x[:length])
+        y_hat_.append(xx[:length])
+
+    y_ = np.vstack(y_)
+    y_hat_ = np.vstack(y_hat_)
+
+    return eval_metrics(y_, y_hat_)
---
+++ test/test_score.py
@@ -0,0 +1,170 @@
+from unittest import TestCase
+
+import numpy as np
+
+from mlutils import score
+
+
+class TestL2Norm(TestCase):
+
+    def test_l2_norm(self):
+
+        x = np.arange(10).reshape(-1, 2)
+        x_hat = np.arange(10).reshape(-1, 2) + 1
+
+        expected = np.ones((5, 1)) * np.sqrt(2.0)
+        result = score.l2_norm(x, x_hat)
+
+        np.testing.assert_array_almost_equal(result, expected)
+
+
+class TestThreshold(TestCase):
+
+    def test_get_threshold_100(self):
+
+        data = np.array([0, 1.])
+
+        expected = 1.
+        result = score.get_threshold(x=data)
+
+        self.assertEqual(result, expected)
+
+    def test_get_threshold_50(self):
+
+        data = np.array([0, 1.])
+
+        expected = 0.5
+        result = score.get_threshold(x=data, p=50)
+
+        self.assertEqual(result, expected)
+
+    def test_get_threshold_80(self):
+
+        data = np.array([0, 1.])
+
+        expected = 0.8
+        result = score.get_threshold(x=data, p=80)
+
+        self.assertEqual(result, expected)
+
+    def test_apply_threshold(self):
+
+        data = np.array([0, 0.4, 0.6, 1.])
+        expected = np.array([0, 0, 1, 1])
+
+        result = score.apply_threshold(x=data, t=0.5)
+
+        np.testing.assert_array_equal(result, expected)
+
+
+class TestEval(TestCase):
+
+    def test_eval_softmax(self):
+
+        data = np.array([0.2, 0.3, 0.5])
+        expected = np.array([[2]], dtype=np.int64)
+
+        result = score.eval_softmax(data)
+
+        np.testing.assert_array_equal(result, expected)
+
+    def test_eval_softmax_2d(self):
+
+        data = np.array([[0.2, 0.3, 0.5], [0.1, 0.8, 0.1]])
+        expected = np.array([[2], [1]], dtype=np.int64)
+
+        result = score.eval_softmax(data)
+
+        np.testing.assert_array_equal(result, expected)
+
+    def test_eval_softmax_3d(self):
+
+        data = np.array([[[0.2, 0.3, 0.5], [0.1, 0.8, 0.1]]])
+        expected = np.array([[2], [1]], dtype=np.int64)
+
+        result = score.eval_softmax(data)
+
+        np.testing.assert_array_equal(result, expected)
+
+    def test_eval_sigmoid(self):
+
+        data = np.array([[0.2], [0.6]])
+        expected = np.array([[0], [1]])
+
+        result = score.eval_sigmoid(y=data)
+
+        np.testing.assert_array_equal(result, expected)
+
+    def test_eval_sigmoid_3d(self):
+
+        data = np.array([[[0.2], [0.6]]])
+        expected = np.array([[0], [1]])
+
+        result = score.eval_sigmoid(y=data)
+
+        np.testing.assert_array_equal(result, expected)
+
+    def test_eval_sigmoid_invert(self):
+
+        data = np.array([[0.2], [0.6]])
+        expected = np.array([[1], [0]])
+
+        result = score.eval_sigmoid(y=data, invert=True)
+
+        np.testing.assert_array_equal(result, expected)
+
+    def test_eval_sigmoid_3d_invert(self):
+
+        data = np.array([[[0.2], [0.6]]])
+        expected = np.array([[1], [0]])
+
+        result = score.eval_sigmoid(y=data, invert=True)
+
+        np.testing.assert_array_equal(result, expected)
+
+
+class TestMovingAverage(TestCase):
+
+    def test_moving_average(self):
+
+        data = np.arange(5)
+        expected = [0, 0.5, 1.5, 2.5, 3.5, 2]
+        result = score.moving_average(data, 2, mode='full')
+
+        np.testing.assert_array_equal(result, expected)
+
+
+class TestMetrics(TestCase):
+
+    def test_eval_metrics(self):
+
+        y = np.ones((10, 1))
+        y_hat = np.ones((10, 1))
+
+        expected = {'accuracy': 1.0, 'precision': 1.0,
+                    'recall': 1.0, 'f1': 1.0}
+        result = score.eval_metrics(y, y_hat)
+
+        self.assertDictEqual(result, expected)
+
+    def test_eval_metrics_uneven_length(self):
+
+        y = np.ones((10, 1))
+        y_hat = np.ones((12, 1))
+
+        expected = {'accuracy': 1.0, 'precision': 1.0,
+                    'recall': 1.0, 'f1': 1.0}
+        result = score.eval_metrics(y, y_hat)
+
+        self.assertDictEqual(result, expected)
+
+    def test_eval_metrics_all(self):
+
+        y = [np.ones((10, 1)), np.zeros((10, 1))]
+        y_hat = [np.ones((10, 1)), np.zeros((10, 1))]
+
+        expected = {'accuracy': 1.0, 'precision': 1.0,
+                    'recall': 1.0, 'f1': 1.0, 'AUC': 1.0}
+        result = score.eval_metrics_all(y, y_hat)
+
+        self.assertDictEqual(result, expected)
