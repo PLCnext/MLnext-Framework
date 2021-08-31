bd7af1e82881bd08dd2dcb420ac990a166f0dc43
GPlatz <36087062+GPla@users.noreply.github.com>
2021-08-31 21:32:41 +0200

    updated metadata and docs

--- mlnext/__init__.py
+++ mlnext/__init__.py
@@ -1,5 +1,17 @@
+"""Machine learning utilities for Tensorflow/Keras.
+"""
 from .data import *  # noqa
 from .io import *  # noqa
 from .pipeline import *  # noqa
 from .plot import *  # noqa
 from .score import *  # noqa
+
+__version__ = '0.0.1dev0'
+
+__title__ = 'MLnext'
+__description__ = 'Machine learning utilities for Tensorflow/Keras.'
+
+__author__ = 'Phoenix Contact Electronics GmbH'
+__email__ = 'digitalfactorynow@phoenixcontact.com'
+
+__copyright__ = f'Copyright (c) 2021 {__author__}'
