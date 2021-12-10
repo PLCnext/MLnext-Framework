b0e1afd4243410ca296864a0bf15df86e9d08a16
gplatz <gplatz@phoenixcontact.com>
2021-12-10 14:50:28 +0100

    added pr_curve

--- test/test_score.py
+++ test/test_score.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from unittest import TestCase
 
 import numpy as np
@@ -6,6 +7,8 @@ import scipy.stats
 
 from mlnext import score
 from mlnext.score import kl_divergence
+from mlnext.score import pr_curve
+from mlnext.score import PRCurve
 
 
 class TestL2Norm(TestCase):
@@ -262,3 +265,58 @@ def test_kl_divergence(
         prior_mean=prior_mean,
         prior_std=prior_std
     ), exp)
+
+
+@pytest.mark.parametrize(
+    'y_true,y_score,exp',
+    [
+        ([0, 0, 1, 1], [0.1, 0.4, 0.35, 0.8],
+         PRCurve(
+            tps=np.array([2., 1., 1.]),
+            fns=np.array([0., 1., 1.]),
+            tns=np.array([1., 1., 2.]),
+            fps=np.array([1., 1., 0.]),
+
+            precision=np.array([0.6667, 0.5000, 1.0000]),
+            recall=np.array([1.0000, 0.5000, 0.5000]),
+            thresholds=np.array([0.35, 0.4, 0.8])
+
+        )),
+        ([0, 0, 1, 1, 1], [0.1, 0.4, 0.5, 0.35, 0.8],
+         PRCurve(
+            tps=np.array([3., 2., 2., 1.]),
+            fns=np.array([0., 1., 1., 2.]),
+            tns=np.array([1., 1., 2., 2.]),
+            fps=np.array([1., 1., 0., 0.]),
+
+            precision=np.array([0.75, 0.6667, 1.0000, 1.0000]),
+            recall=np.array([1.0000, 0.6667, 0.6667, 0.3333]),
+            thresholds=np.array([0.35, 0.4, 0.5, 0.8])
+
+        ))
+    ]
+)
+def test_pr_curve(
+    y_true: np.ndarray,
+    y_score: np.ndarray,
+    exp: PRCurve
+):
+
+    result = pr_curve(np.array(y_true), np.array(y_score))
+
+    for field in fields(exp):
+        r, e = getattr(result, field.name), getattr(exp, field.name)
+        np.testing.assert_almost_equal(r, e, decimal=4)
+
+    # confusion matrix
+    for i, cm in enumerate(result):
+        np.testing.assert_almost_equal(cm.TP, exp.tps[i])
+        np.testing.assert_almost_equal(cm.FN, exp.fns[i])
+        np.testing.assert_almost_equal(cm.TN, exp.tns[i])
+        np.testing.assert_almost_equal(cm.FP, exp.fps[i])
+
+        np.testing.assert_almost_equal(
+            cm.precision, exp.precision[i], decimal=4)
+        np.testing.assert_almost_equal(cm.recall, exp.recall[i], decimal=4)
+        np.testing.assert_almost_equal(cm.f1, exp.f1[i], decimal=4)
+        np.testing.assert_almost_equal(cm.accuracy, exp.accuracy[i], decimal=4)
