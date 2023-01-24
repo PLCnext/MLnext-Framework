847515b9926ffa0e90d2567c64c6961b8022689c
Gorden Platz <gplatz@phoenixcontact.com>
2023-01-24 15:26:24 +0100

    Resolve "Add `recall_anomalies`"

--- mlnext/score.py
+++ mlnext/score.py
@@ -7,15 +7,20 @@ from typing import Dict
 from typing import Iterator
 from typing import List
 from typing import Optional
+from typing import Tuple
 from typing import Union
 
 import numpy as np
 import pandas as pd
+from deprecate import deprecated
 from sklearn import metrics
 from sklearn.metrics import auc
 from sklearn.metrics._ranking import _binary_clf_curve
 
+from .anomaly import _recall_anomalies
 from .anomaly import apply_point_adjust
+from .anomaly import find_anomalies
+from .anomaly import recall_anomalies
 from mlnext.utils import check_ndim
 from mlnext.utils import check_size
 from mlnext.utils import truncate
@@ -266,7 +271,7 @@ def moving_average(x: np.ndarray, step: int = 10, mode='full') -> np.ndarray:
 
 
 def eval_metrics(y: np.ndarray, y_hat: np.ndarray) -> Dict[str, float]:
-    """Calculates accuracy, f1, precision and recall.
+    """Calculates accuracy, f1, precision, recall and recall_anomalies.
 
     Arguments:
         y (np.ndarray): Ground truth labels.
@@ -279,17 +284,18 @@ def eval_metrics(y: np.ndarray, y_hat: np.ndarray) -> Dict[str, float]:
         >>> y, y_hat = np.ones((10, 1)), np.ones((10, 1))
         >>> eval_metrics(y, y_hat)
         {'accuracy': 1.0, 'precision': 1.0, 'recall': 1.0, 'f1': 1.0,
-         'roc_auc': 1.0}
+         'anomalies': 1.0}
     """
     scores = {
         'accuracy': metrics.accuracy_score,
         'precision': metrics.precision_score,
         'recall': metrics.recall_score,
-        'f1': metrics.f1_score
+        'f1': metrics.f1_score,
+        'anomalies': recall_anomalies
     }
 
     if y.shape != y_hat.shape:
-        warnings.warn(f'Shapes unaligned {y.shape} and {y_hat.shape}.')
+        warnings.warn(f'Shapes unaligned y {y.shape} and y_hat {y_hat.shape}.')
 
     (y, y_hat), = truncate((y, y_hat))
     results = {}
@@ -326,11 +332,16 @@ def eval_metrics_all(
          'roc_auc': 1.0}
     """
     if len(y) != len(y_hat):
-        raise ValueError('y and y_hat must have the same number elements.')
+        raise ValueError(
+            'y and y_hat must have the same number elements, '
+            f'but found y={len(y)} and y_hat={len(y_hat)}.'
+        )
 
     # allow 1d or 2d arrays with the 2nd dimension of 1
-    check_ndim(*y, *y_hat, ndim=2, strict=False)
-    check_size(*y, *y_hat, size=1, axis=1, strict=False)
+    check_ndim(*y, ndim=2, strict=False)
+    check_ndim(*y_hat, ndim=2, strict=False)
+    check_size(*y, size=1, axis=1, strict=False)
+    check_size(*y_hat, size=1, axis=1, strict=False)
 
     y = list(map(lambda x: x.reshape(-1), y))
     y_hat = list(map(lambda x: x.reshape(-1), y_hat))
@@ -355,11 +366,15 @@ class ConfusionMatrix:
         that are correctly assigned to negative class.
       FP (int): true negatives, the number of samples from the negative class
         that are wrongly assigned to the positive class.
+      DA (int): detected anomalies segments by at least one point.
+      TA (int): total number of anomaly segments.
     """
     TP: int = 0  # True Positives
     FN: int = 0  # False Negatives
     TN: int = 0  # True Negative
     FP: int = 0  # False Positive
+    DA: int = 0  # Detected anomalies
+    TA: int = 0  # total number of anomalies
 
     def __add__(self, cm: 'ConfusionMatrix') -> 'ConfusionMatrix':
         """Overrides the add operator.
@@ -372,7 +387,9 @@ class ConfusionMatrix:
             TP=self.TP + cm.TP,
             FN=self.FN + cm.FN,
             TN=self.TN + cm.TN,
-            FP=self.FP + cm.FP
+            FP=self.FP + cm.FP,
+            DA=self.DA + cm.DA,
+            TA=self.TA + cm.TA
         )
 
     def __str__(self) -> str:
@@ -429,6 +446,15 @@ class ConfusionMatrix:
         return ((2 * self.precision * self.recall) /
                 (self.precision + self.recall))
 
+    @property
+    def recall_anomalies(self) -> float:
+        """Calculates the percentage of detected anomaly segments.
+
+        Returns:
+            float: Returns the percentage of detected segments.
+        """
+        return self.DA / self.TA
+
     def metrics(self) -> Dict[str, float]:
         """Returns all metrics.
 
@@ -439,7 +465,8 @@ class ConfusionMatrix:
             'accuracy': self.accuracy,
             'f1': self.f1,
             'recall': self.recall,
-            'precision': self.precision
+            'precision': self.precision,
+            'anomalies': self.recall_anomalies
         }
 
 
@@ -459,6 +486,9 @@ class PRCurve:
           number of negative samples assigned a score < thresholds[i].
         fps (np.ndarray): A count of false positives, at index i being the
           number of negative samples assigned a score >= thresholds[i].
+        das (np.ndarray): A count of detected anomaly segments, at index i
+          being the number of detected anomalies for a score >= thresholds[i].
+        tas (np.ndarray): Total number of anomaly segments.
         precision (np.ndarray): Precision values such that element i is the
           precision of predictions with score >= thresholds[i].
         recall (np.ndarray): Decreasing recall values such that element i is
@@ -471,6 +501,8 @@ class PRCurve:
     fns: np.ndarray  # false negatives
     tns: np.ndarray  # true negatives
     fps: np.ndarray  # false positives
+    das: np.ndarray  # detected anomaly segments
+    tas: np.ndarray  # total number of a anomaly segments
 
     precision: np.ndarray
     recall: np.ndarray
@@ -483,15 +515,20 @@ class PRCurve:
             str: Returns a string respresentation of the pr-curve.
         """
 
-        header = (' {:^6s} ' * 9).format(
-            'TH', 'ACC', 'F1', 'PRC', 'RCL', 'TP', 'FN', 'TN', 'FP')
+        header = (' {:^6s} ' * 12).format(
+            'TH', 'ACC', 'F1', 'PRC', 'RCL', 'ANO',
+            'TP', 'FN', 'TN', 'FP', 'DA', 'TA'
+        )
 
-        fmt = ' {:^6.4f} ' * 5 + ' {:^6.0f} ' * 4
+        fmt = ' {:^6.4f} ' * 6 + ' {:^6.0f} ' * 6
         rows = [
             fmt.format(*row)
             for row in
-            zip(self.thresholds, self.accuracy, self.f1, self.precision,
-                self.recall, self.tps, self.fns, self.tns, self.fps)
+            zip(
+                self.thresholds, self.accuracy, self.f1, self.precision,
+                self.recall, self.recall_anomalies,
+                self.tps, self.fns, self.tns, self.fps, self.das, self.tas
+            )
         ]
         return '\n'.join([header, *rows, f'AUC: {self.auc:.4f}'])
 
@@ -516,7 +553,9 @@ class PRCurve:
             TP=self.tps[i],
             FN=self.fns[i],
             TN=self.tns[i],
-            FP=self.fps[i]
+            FP=self.fps[i],
+            DA=self.das[i],
+            TA=self.tas[i]
         )
 
     def __len__(self) -> int:
@@ -570,6 +609,15 @@ class PRCurve:
         return ((2 * self.precision * self.recall) /
                 (self.precision + self.recall))
 
+    @property
+    def recall_anomalies(self) -> np.ndarray:
+        """Calculates the fraction of detected anomaly segments.
+
+        Returns:
+            np.ndarray: Returns the anomaly recall.
+        """
+        return self.das / self.tas
+
     def to_tensorboard(self) -> Dict[str, Any]:
         """Converts the container to keyword arguments for Tensorboard.
         See https://github.com/tensorflow/tensorboard/blob/master/tensorboard/plugins/pr_curve/README.md.
@@ -589,10 +637,17 @@ class PRCurve:
         }
 
 
+@deprecated(
+    True,
+    args_mapping={'y_true': 'y'},
+    deprecated_in='0.4',
+    remove_in='0.6',
+)
 def pr_curve(
-    y_true: np.ndarray,
+    y: np.ndarray,
     y_score: np.ndarray,
     *,
+    y_true: Optional[np.ndarray] = None,
     pos_label: Optional[Union[str, int]] = None,
     sample_weight: Optional[Union[List, np.ndarray]] = None
 ) -> PRCurve:
@@ -607,11 +662,11 @@ def pr_curve(
     Furthermore, we can you use results for further processing.
 
     Args:
-        y_true (np.ndarray): Positive lables either {-1, 1} or {0, 1}.
+        y (np.ndarray): Positive lables either {-1, 1} or {0, 1}.
           Otherwise, pos_label needs to be given.
         y_score (np.ndarray):  Target scores in range [0, 1].
         pos_label (int, optional):The label of the positive class.
-          When pos_label=None, if y_true is in {-1, 1} or {0, 1},
+          When pos_label=None, if y is in {-1, 1} or {0, 1},
           pos_label is set to 1, otherwise an error will be raised.
           Defaults to None.
         sample_weight (Union[List, np.ndarray], optional): Sample weights.
@@ -623,14 +678,15 @@ def pr_curve(
     Example:
         >>> import numpy as np
         >>> from mlnext.score import pr_curve
-        >>> y_true = np.array([0, 0, 1, 1])
+        >>> y = np.array([0, 0, 1, 1])
         >>> y_scores = np.array([0.1, 0.4, 0.35, 0.8])
-        >>> curve = pr_curve(y_true, y_scores)
+        >>> curve = pr_curve(y, y_scores)
         >>> print(curve)
-          TH     ACC      F1     PRC     RCL      TP      FN      TN      FP
-        0.3500  0.7500  0.8000  0.6667  1.0000    2       0       1       1
-        0.4000  0.5000  0.5000  0.5000  0.5000    1       1       1       1
-        0.8000  0.7500  0.6667  1.0000  0.5000    1       1       2       0
+          TH     ACC      F1     PRC     RCL     ANO      TP      FN      TN      FP      DA      TA
+        0.3500  0.7500  0.8000  0.6667  1.0000  1.0000    2       0       1       1       1       1
+        0.4000  0.5000  0.5000  0.5000  0.5000  1.0000    1       1       1       1       1       1
+        0.8000  0.7500  0.6667  1.0000  0.5000  1.0000    1       1       2       0       1       1
+        AUC: 0.7917
 
         >>> # access fields
         >>> print(curve.f1, curve.thresholds)
@@ -661,7 +717,7 @@ def pr_curve(
     """  # noqa
 
     fps, tps, thresholds = _binary_clf_curve(
-        y_true, y_score, pos_label=pos_label, sample_weight=sample_weight
+        y, y_score, pos_label=pos_label, sample_weight=sample_weight
     )
 
     fns = tps[-1] - tps
@@ -676,8 +732,15 @@ def pr_curve(
     last_ind = tps.searchsorted(tps[-1])
     sl = slice(last_ind, None, -1)
 
+    das, tas = _recall_anomalies_curve(
+        y,
+        y_score,
+        thresholds=thresholds,
+        pos_label=pos_label
+    )
+
     return PRCurve(
-        tps[sl], fns[sl], tns[sl], fps[sl],
+        tps[sl], fns[sl], tns[sl], fps[sl], das[sl], tas[sl],
         precision[sl], recall[sl], thresholds[sl]
     )
 
@@ -775,3 +838,29 @@ def auc_point_adjust_metrics(
         f'auc_{column}': auc(df.index, df[column]) / 100
         for column in df
     }
+
+
+def _recall_anomalies_curve(
+    y: np.ndarray,
+    y_score: np.ndarray,
+    *,
+    thresholds: List[float],
+    pos_label: Optional[Union[str, int]] = None,
+    k: float = 0
+) -> Tuple[np.ndarray, np.ndarray]:
+    # determine positive and negative labels
+    pos_label = 1 if pos_label is None else pos_label
+    y = y == pos_label
+
+    anomalies = find_anomalies(y)
+
+    # placeholder
+    detected = np.zeros(len(thresholds))
+    total = np.ones(len(thresholds)) * len(anomalies)
+
+    # count segments where at least k% are detected
+    for i, threshold in enumerate(thresholds):
+        y_hat = apply_threshold(y_score, threshold=threshold)
+        detected[i] = _recall_anomalies(anomalies, y_hat, k=k)
+
+    return detected, total
